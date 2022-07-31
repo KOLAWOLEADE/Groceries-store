b8ae0f3ba59232895095df17756073ad04ffbc4b
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-07-31 14:02:59 +0100

    add Groceries-stores

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
     $(".Groceries").submit(function(event){
         const item1Input = $("#item1").val();
-        const item2Input = $("#item2").val ();
-        const item3Input  = $("#item3") .val();
+        const item2Input = $("#item2").val();
+        const item3Input = $("#item3").val();
         const item4Input = $("#item4").val();
-        const item5Input    = $("#item5").val();
+        const item5Input = $("#item5").val();
 
 
-        $(".item1").text(item1Input);
-        $(".item2").text(item2Input);
-        $(".item3").text(item3Input);
-        $(".item4").text(item4Input);
-        $(".item5").text(item5Input);
+        $(".item1").text(item1Input.toUpperCase());
+        $(".item2").text(item2Input.toUpperCase());
+        $(".item3").text(item3Input.toUpperCase());
+        $(".item4").text(item4Input.toUpperCase());
+        $(".item5").text(item5Input.toUpperCase());
 
 
         $(".items").show();
+      
 
-        event.preventDefault();
-       
-        
+        event.preventDefault();      
 
   });
 });
